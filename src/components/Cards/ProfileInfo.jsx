ec00f1f1f2130a48f976c5e46a186c12a9165163
ohey <huynhgiabao853@gmail.com>
2025-03-20 15:42:31 +0700

    popup-payment

--- src/components/Cards/ProfileInfo.jsx
+++ src/components/Cards/ProfileInfo.jsx
@@ -1,36 +1,22 @@
 import PropTypes from "prop-types";
 import { IoMdLogOut } from "react-icons/io";
 import { FaUserCircle, FaRegCommentDots } from "react-icons/fa";
-import api from "../../services/api";
-const ProfileInfo = ({ onLogout }) => {
+import { useDispatch } from "react-redux";
+import { openPopup } from "../../redux/user/paymentSlice";
 
-  const handlePayment = async () => {
-    try {
-      const orderCode = Date.now();
-      const res = await api.post("/payment/create-payment", {
-        orderCode,
-        amount: 2000,
-        description: "Nap tien Memmomind",
-        cancelUrl: `${window.location.origin}/payment/cancel`,
-        returnUrl: `${window.location.origin}/payment/success`,
-      });
-      console.log("oderCode", orderCode);
-      if (res.data?.data?.checkoutUrl) {
-        window.location.href = res.data.data.checkoutUrl;
-      }
-    } catch (error) {
-      console.error("Lỗi thanh toán:", error);
-    }
-  };
+const ProfileInfo = ({ onLogout }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="flex items-center gap-3">
 
       <button
-        onClick={handlePayment}
+        onClick={() => dispatch(openPopup())} // Mở popup
         className="flex items-center justify-center gap-1 w-24 h-10 bg-gradient-to-r from-blue-400 to-blue-600 text-white text-sm font-medium rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105"
       >
         💰 Nạp tiền
       </button>
+
       {/* Nút Feedback */}
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLSeZ1Mi2hHs5R0w5BLYHj8JcPZxWNzJPoentatCOlQitua58tw/viewform"
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // 🔹 Base URL của API
-const API_BASE_URL = "https://memmomindbe-test-jgcl.onrender.com/api";
+const API_BASE_URL = "https://memmomind-be-ycwv.onrender.com/api";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
